8a33d0608fc411994a86ac6233e2d80779e6eb5e
Aaron Bieber <aaron@bolddaemon.com>
2015-11-24 18:45:48 -0700

    rename MMM define and disable leveling

--- 151117_ConfiguredMarlin/Marlin/Configuration.h
+++ 151117_ConfiguredMarlin/Marlin/Configuration.h
@@ -34,8 +34,7 @@
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
   #define MOTHERBOARD BOARD_RAMPS_13_EFB
-  #define MMM_ADVANCE_PIN A3	// The pin for our extrusion advance knob, value read from
-  				// pin is used as a multiplier for extrusion
+  #define MMM_KNOB_PIN A3	// The pin for our pot, value read from pin is used as a multiplier for extrusion
 #endif
 
 // Define this to set a custom name for your generic Mendel,
@@ -340,7 +339,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define Z_MAX_LENGTH (Z_MAX_POS - Z_MIN_POS)
 //============================= Bed Auto Leveling ===========================
 
-#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
+//#define ENABLE_AUTO_BED_LEVELING // Delete the comment to enable (remove // at the start of the line)
 #define Z_PROBE_REPEATABILITY_TEST  // If not commented out, Z-Probe Repeatability test will be included if Auto Bed Leveling is Enabled.
 
 #ifdef ENABLE_AUTO_BED_LEVELING
